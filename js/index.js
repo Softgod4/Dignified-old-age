005ac837c60a728815ebd0e122e3bc07837d8aae
Softgodworld <softgod2005@gmail.com>
2023-06-12 21:56:35 +0300

    добавил

--- js/index.js
+++ js/index.js
@@ -4,4 +4,22 @@ document.querySelector('a').addEventListener('click', function(e) {
       behavior: 'smooth'
     });
   });
-console.log('hello world')
+
+
+  function openModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+    
+    // Обработчик для закрытия модального окна при клике на крестик
+    var span = document.getElementsByClassName("close")[0];
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    // Обработчик для закрытия модального окна при клике вне его области
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+  }
