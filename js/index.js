f01f6b74be972924bdec5433938f7a0d1c2d86b8
Softgodworld <softgod2005@gmail.com>
2023-06-14 15:59:16 +0300

    добавлены попапы и тд

--- js/index.js
+++ js/index.js
@@ -10,16 +10,38 @@ document.querySelector('a').addEventListener('click', function(e) {
     var modal = document.getElementById("myModal");
     modal.style.display = "block";
     
-    // Обработчик для закрытия модального окна при клике на крестик
     var span = document.getElementsByClassName("close")[0];
     span.onclick = function() {
       modal.style.display = "none";
     }
 
-    // Обработчик для закрытия модального окна при клике вне его области
     window.onclick = function(event) {
       if (event.target == modal) {
         modal.style.display = "none";
       }
     }
   }
+  function openModal2() {
+    var modal = document.getElementById("myModal2");
+    modal.style.display = "block";
+    
+    var span = document.getElementsByClassName("close1")[0];
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+  }
+
+window.addEventListener('scroll', function() {
+  const navbar = document.getElementById('navbar');
+  if (window.scrollY >= 300) { /* если пользователь уже прокрутил на 50px и более */
+    navbar.classList.add('navbar-scrolled'); /* добавляем класс для изменения цвета фона навигационной панели */
+  } else {
+    navbar.classList.remove('navbar-scrolled'); /* удаляем класс, чтобы вернуть начальный цвет фона навигационной панели */
+  }
+});
