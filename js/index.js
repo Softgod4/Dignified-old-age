0338293b84f1ea4c12f2bcfe6193285b8caaa07f
Softgodworld <softgod2005@gmail.com>
2023-06-15 15:13:16 +0300

    есть изменения

--- js/index.js
+++ js/index.js
@@ -37,22 +37,103 @@ document.querySelector('a').addEventListener('click', function(e) {
     }
   }
 
+// window.addEventListener('scroll', function() {
+//   const navbar = document.getElementById('navbar');
+//   if (window.scrollY >= 300) { /* если пользователь уже прокрутил на 50px и более */
+//     navbar.classList.add('navbar-scrolled'); /* добавляем класс для изменения цвета фона навигационной панели */
+//   } else {
+//     navbar.classList.remove('navbar-scrolled'); /* удаляем класс, чтобы вернуть начальный цвет фона навигационной панели */
+//   }
+// });
+
+
+
+
 window.addEventListener('scroll', function() {
-  const navbar = document.getElementById('navbar');
-  if (window.scrollY >= 300) { /* если пользователь уже прокрутил на 50px и более */
-    navbar.classList.add('navbar-scrolled'); /* добавляем класс для изменения цвета фона навигационной панели */
+  var navbar = document.getElementsByClassName('navbar-container')[0];
+  if (window.scrollY > 0) {
+    navbar.classList.add('scrolled');
   } else {
-    navbar.classList.remove('navbar-scrolled'); /* удаляем класс, чтобы вернуть начальный цвет фона навигационной панели */
+    navbar.classList.remove('scrolled');
   }
 });
 
 
 $('.slider').slick({
-  dots: true,
+  dots: false,
   infinite: false,
   speed: 300,
   slidesToShow: 4,
   slidesToScroll: 4,
+  prevArrow: '<button type="button" class="slick-prev"><img src="img/icon_arrow_slider.png" /></button>',
+  nextArrow: '<button type="button" class="slick-next"><img src="img/icon_arrow_slider.png" /></button>',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+
+$('.slider2').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+$('.slider3').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
   responsive: [
     {
       breakpoint: 1024,
